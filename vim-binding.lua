ecd59efd81b5011240da933a9cf6f69fdb45507b
S1ngS1ng <liuxing0514@gmail.com>
2016-12-06 23:54:04 -0600

    chore(vim-binding): Initial version of key-map feature

--- vim-binding.lua
+++ vim-binding.lua
@@ -1,18 +1,27 @@
-local modifierListener = hs.eventtap.new({ hs.eventtap.event.types.flagsChanged }, function(event)
-  local modFlags = event:getFlags()
-  local modTable = {}
-  if modFlags['ctrl'] then
-    for flag in pairs(modFlags) do
-      table.insert(modTable, flag)
-    end
-    hs.eventtap.event.newKeyEvent(modTable, vimBinding[key], true):post()
-  end
-end)
-modifierListener:start()
-
 local vimBinding = {
   h = 'left',
   j = 'down',
   k = 'up',
   l = 'right'
 }
+
+local modifierListener = hs.eventtap.new({ hs.eventtap.event.types.flagsChanged }, function(event)
+  local modFlags = event:getFlags()
+  local modTable = {}
+  if modFlags['ctrl'] then
+    local keyDown
+    local keyListener = hs.eventtap.new({ hs.eventtap.event.types.keyDown }, function(event)
+      local keyDown = hs.keycodes.map[event:getKeyCode()]
+      if keyDown == 'h' or 'j' or 'k' or 'l' then
+        print(keyDown)
+        keyListener:stop()
+          hs.eventtap.event.newKeyEvent(modTable, vimBinding.keyDown, true):post()
+      end
+    end)
+    for flag in pairs(modFlags) do
+      table.insert(modTable, flag)
+    end
+    print(keyListener:start())
+  end
+end)
+modifierListener:start()
